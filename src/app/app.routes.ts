f61b024f7d6be6bb214091501f2e8f119b3ac631
Вова Якорев <vovanyakorev2@gmail.com>
2024-03-17 10:44:29 +0100

    feat(EmployeeList): configure employee list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './pages/employee-list/employee-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: EmployeeListComponent },
+];
